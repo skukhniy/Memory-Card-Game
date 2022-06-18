2420643471daa0cc98a2150a62a2117dba1aecc7
Stan <skukhniy@gmail.com>
2022-06-17 23:24:26 -0400

    added background + header component + star wars font

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div>
+      <h2>Star Wars</h2>
+      <h2>Memory Game</h2>
+    </div>
+  );
+}
